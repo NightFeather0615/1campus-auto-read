1cb61275799121fe45ec80e2174f9bd47658e74f
NightFeather <leolee50910@gmail.com>
2023-03-12 03:20:27 +0800

    fix

--- functions/handle-cors.ts
+++ functions/handle-cors.ts
@@ -1,8 +1,11 @@
 import type { Handler } from "@netlify/functions";
 
+const countPerPage = 100;
+const pageNo = 1;
+
 const handler: Handler = async (event, _) => {
   if (
-    event.queryStringParameters == null || event.queryStringParameters.access_token == undefined || event.queryStringParameters.message_ids == undefined
+    event.queryStringParameters == null || event.queryStringParameters.access_token == undefined
   ) {
     return {
       statusCode: 400,
@@ -15,25 +18,38 @@ const handler: Handler = async (event, _) => {
       }),
     };
   }
-  event.queryStringParameters.message_ids.split(",").forEach(async (message_id) => {
-    await fetch(
-      "https://msg.ischool.com.tw/services/personal/messages",
-      {
-          method: "PUT",
-          body: `access_token=${event.queryStringParameters!.access_token}&message_id=${message_id}`,
-          headers: {
-              "Host": "msg.ischool.com.tw",
-              "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 11; sdk_gphone_arm64 Build/RSR1.210722.002)",
-              "Origin": "https://msg.ischool.com.tw/",
-              "Content-Type": "application/x-www-form-urlencoded"
-          }
-      }
-    )
-  });
+
+  // let messages = await (await fetch(
+  //   `https://msg.ischool.com.tw/services/personal/messages?access_token=${event.queryStringParameters!.access_token}&count_per_page=${countPerPage}&page_no=${pageNo}`,
+  //   {
+  //       method: "GET"
+  //   }
+  // )).json();
+
+  // messages
+  //   .filter(messageData => messageData.read_time == null)
+  //   .map(messageData => messageData.message_id)
+  //   .forEach(async (messageId) => {
+  //     await fetch(
+  //       "https://msg.ischool.com.tw/services/personal/messages",
+  //       {
+  //           method: "PUT",
+  //           body: `access_token=${event.queryStringParameters!.access_token}&message_id=${messageId}`,
+  //           headers: {
+  //               "Host": "msg.ischool.com.tw",
+  //               "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 11; sdk_gphone_arm64 Build/RSR1.210722.002)",
+  //               "Origin": "https://msg.ischool.com.tw/",
+  //               "Content-Type": "application/x-www-form-urlencoded"
+  //           }
+  //       }
+  //     )
+  //   });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ success: true }),
+    body: `<html><head><meta charset="utf-8"></head><script>alert("已讀最近100則訊息！感謝你使用 1Campus Auto Read\\n本功能由個人維護，請只在必要時使用\\n※此書籤僅供學術用途")</script></html>`,
     headers: {
+      'Content-Type': 'text/html, charset=utf-8',
       'Access-Control-Allow-Origin': '*'
     }
   };
